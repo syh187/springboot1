388a1a2dbd87010393c82fafa99a18ac7d0a2fb4
syh <2115946114@qq.com>
2024-03-07 17:09:21 +0800

    first commit

---
+++ src/main/java/com/syh/boot/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.syh.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloController() {
+        return "hello springboot";
+    }
+
+}
