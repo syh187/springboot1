d7ebbf8816125bd5c89e1a8b060ed865a710d7dc
syh <2115946114@qq.com>
2024-03-07 17:41:59 +0800

    master commit1

--- src/main/java/com/syh/boot/controller/HelloController.java
+++ src/main/java/com/syh/boot/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String helloController() {
-        return "hello springboot322";
+        return "hello master";
     }
 
 }
